f1ce727f43525da5704b5987d177affbe71160a1
Anna Shnir <shnir.anna@gmail.com>
2015-07-28 16:47:37 -0400

    added columns click events

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-script.js
+$(document).ready(function() {
+	$('.green').click(function() {
+		alert('You have clicked the Green column!')
+	});
+
+	$('.blue').click(function() {
+		var name = prompt('Enter your name:');
+		console.log(name);
+	});
+
+
+	
+});
